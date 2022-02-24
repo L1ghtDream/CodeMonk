983d00aad943658c063e8e8ee7fd8dfaa5253f24
L1ghtDream <radu.voinea.mihai@gmail.com>
2022-02-24 22:05:29 +0200

    The unlucky 13 - WIP

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -17,25 +18,27 @@ int main() {
     return 0;
 }
 
+/*
+ 2 -> xx -> 13 -> -1
+ 3 -> xxx -> x13, 13x -> -20
+ 4 -> xxxx -> xx13 x13x 13xx -> - 300
+ 5 -> xxxxx -> xxx13 xx13x x13xx 13xxx -> -4000
+
+ 1 -> 10 <-> (10 ^ 1) - (1 - 1)
+ 2 -> 99 <-> (10 ^ 2) - (2 - 1)
+ 3 -> 198 <-> (10 ^ 3) - (3 - 1)
+ ...
+ 9 -> 1[9]2 <-> (10 ^ 9) - (9 - 1) -> 999999983
+ 10 -> 1[9]1 <-> (10 ^ 10) - (10 - 1) -> 999999820
+ 11 -> 1[...]90 -> 999998199
+ 12 -> 1[...]89 -> 999981998
+
+
+
+ */
+
 void execute() {
     int n;
-    vector<int> v;
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        int tmp;
-        cin >> tmp;
-        v.push_back(tmp);
-    }
 
-    sort(v.begin(), v.end());
-
-    int minn = 100000000;
-    for (int i = 0; i < n - 1; i++) {
-        int tmp = v[i] ^ v[i + 1];
-        if (minn > tmp) {
-            minn = tmp;
-        }
-    }
-
-    cout << minn << endl;
 }
